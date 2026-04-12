94567bd89642dc321029d4047cbb6017744b3932
Vipul Choudhary <choudharyvipul722@gmail.com>
2026-04-12 22:01:03 +0530

    changes in deleteAllProduct By Id

--- src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
+++ src/main/java/com/ecommerce/project/repositories/CartItemRepository.java
@@ -4,6 +4,9 @@ import com.ecommerce.project.model.CartItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.List;
 
 public interface CartItemRepository  extends JpaRepository<CartItem, Long> {
 
@@ -17,6 +20,16 @@ public interface CartItemRepository  extends JpaRepository<CartItem, Long> {
     @Modifying
     @Query("DELETE FROM CartItem ci WHERE ci.cart.id = ?1")
     void deleteAllByCartId(Long cartId);
+
+
+
+    @Modifying(clearAutomatically = true)
+    @Query("DELETE FROM CartItem ci WHERE ci.product.productId = :productId")
+    void deleteAllByProductId(@Param("productId") Long productId);
+
+    @Query("SELECT ci FROM CartItem ci WHERE ci.product.productId = :productId")
+    List<CartItem> findCartItemsByProductId(@Param("productId") Long productId);
+
 
 
 }
