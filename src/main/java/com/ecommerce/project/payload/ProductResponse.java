0d5aacfb85a4342def616e443efeb186914075f3
Vipul Choudhary <choudharyvipul722@gmail.com>
2026-08-06 22:40:03 +0530

    gave the serial id

--- src/main/java/com/ecommerce/project/payload/ProductResponse.java
+++ src/main/java/com/ecommerce/project/payload/ProductResponse.java
@@ -4,12 +4,16 @@ import lombok.Data;
 import lombok.AllArgsConstructor;
 import lombok.NoArgsConstructor;
 
+import java.io.Serial;
+import java.io.Serializable;
 import java.util.List;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class ProductResponse {
+public class ProductResponse implements Serializable {
+    @Serial
+    private static final long serialVersionUID = 1L;
     private List<ProductDTO> content;
     private Integer pageNumber;
     private Integer pageSize;
